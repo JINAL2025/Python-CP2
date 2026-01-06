275f7c18d7c6bb19bd021491e178b453e49346e3
JINAL2025 <chetanpandya2011@gmail.com>
2026-01-06 15:59:32 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,5 @@
+#Python program to calculate the average of marks in 3 subjects
+m1 = int (input("Enter the marks in the first subject : "))
+m2 = int (input("Enter the marks in the second subject : "))
+m3 = int (input("Enter the marks in the third subject : "))
+print("The average marks are : ", (m1 + m2 + m3)/3)
